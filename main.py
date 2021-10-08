6c16d06aa10e08703aff479b2c6ac619388850b8
Omar Macias <maciaseo@gmail.com>
2021-10-08 18:18:05 -0500

    Added quiz loop

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import  QuizBrain
 
 question_bank = []
 
-for item in question_data:
-    n_question = Question(item["text"], item["answer"])
-    question_bank.append(n_question)
+for question in question_data:
+    new_question = Question(question["text"], question["answer"])
+    question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
